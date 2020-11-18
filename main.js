7b35bafdf0b16676cc27018ae0f5ad33960a2bbf
David Castillo <hola@davecastworks.com>
2020-11-18 08:38:44 -0400

    Agregada imagen nueva, y agregado css al proyecyo

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
+/*Utils*/
+const setNumberPokemon = (numberPokemon) => {
+  let newNumber;
+  if (numberPokemon < 100) {
+    newNumber = numberPokemon < 10 ? `00${numberPokemon}` : `0${numberPokemon}`;
+  } else {
+    newNumber = numberPokemon;
+  }
+
+  return newNumber;
+};
+
 function getPokemonById(id) {
   return fetch(`${URL}${id}`)
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       return data;
     });
 }
@@ -12,12 +25,25 @@ function render(pokemon) {
   const $root = document.getElementById("root");
 
   $root.innerHTML = `
-    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
-    <h2>${pokemon.name}</h2>
+    <article class="pokemon">
+      <div class="pokemon__thumbnail">
+        <figure>
+          <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${setNumberPokemon(
+            pokemon.id
+          )}.png" alt="${pokemon.name}" />
+        </figure>
+      </div>
+      <div class="pokemon__info">
+        <h2>${pokemon.name}</h2>
+        <p class="pokemon__number"><strong>#${setNumberPokemon(
+          pokemon.id
+        )}</strong></p>
+      </div>
+    </article>
   `;
 }
 async function init() {
-  const pokemon = await getPokemonById(6);
+  const pokemon = await getPokemonById(53);
   render(pokemon);
 }
 
